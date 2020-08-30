c0f20a178a491fe15d1ee7052c5129cfaee4d8ff
giocarvalho07 <giovannidecarvalhomendes@outloook.com>
2020-08-29 21:08:30 -0300

    criar escopo da aplicação, rotas e método de cadastro e edicao

---
+++ src/test/java/com/listatelefonica/ListaTelefonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listatelefonica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaTelefonicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
